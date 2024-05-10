847e400bfd3d780e5eb01ab579bc86ec80124b60
Jasvanth-Hub <bhukyajasvanth@gmail.com>
2024-05-10 19:02:54 +0530

    deleted some files

--- Level 2 Task-4 - Basic Login Authentication System/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-exports.connectDatabase = ()=>{
-    mongoose.connect(process.env.MONGO_URI).then((con)=>{
-        console.log(`Database Connected : ${con.connection.host}`)
-    }).catch((e)=>{
-        console.log(e)
-    })
-}
